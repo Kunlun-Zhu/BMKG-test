94b96b832706bde7ce4341f4fabf50164dd50655
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2022-04-15 11:14:43 +0800

    Update transr.py

--- bmkg/models/transx/transr.py
+++ bmkg/models/transx/transr.py
@@ -64,7 +64,8 @@ class TransR(TransX):
 	def _transfer_h(self, e, r_transfer):
 		r_transfer = r_transfer.view(-1, self.dim_e, self.dim_r)
 		
-		e = e.view(-1, 1, self.dim_e)
+		#e = e.view(-1, 1, self.dim_e)
+		e = e.view(1, -1, self.dim_e)
 		print(r_transfer.shape)
 		print(e.shape)
 		e = torch.matmul(e, r_transfer)
@@ -76,6 +77,8 @@ class TransR(TransX):
 		if e.shape[0] != r_transfer.shape[0]:
 			#e = e.view(-1, r_transfer.shape[0], self.dim_e).permute(1, 0, 2)
 			e = e.view(self.ent_size, 1, self.dim_e).permute(1, 0, 2)
+			print(r_transfer.shape)
+			print(e.shape)
 			e = torch.matmul(e, r_transfer).permute(1, 0, 2)
 		
 		else:
