200b60b95d0c0d4ba846f1ff21d4cbe719d1121c
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2022-04-15 09:17:20 +0800

    Update transr.py

--- bmkg/models/transx/transr.py
+++ bmkg/models/transx/transr.py
@@ -62,12 +62,18 @@ class TransR(TransX):
 	
 	def _transfer(self, e, r_transfer):
 		r_transfer = r_transfer.view(-1, self.dim_e, self.dim_r)
+		'''
 		if e.shape[0] != r_transfer.shape[0]:
 			e = e.view(-1, r_transfer.shape[0], self.dim_e).permute(1, 0, 2)
 			e = torch.matmul(e, r_transfer).permute(1, 0, 2)
 		else:
 			e = e.view(-1, 1, self.dim_e)
 			e = torch.matmul(e, r_transfer)
+		'''
+		e = e.view(-1, 1, self.dim_e)
+		e = torch.matmul(e, r_transfer)
+		
+
 		return e.view(-1, self.dim_r)
 
 	#scoring function added
