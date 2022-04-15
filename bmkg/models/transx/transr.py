50ef87d115b67689588d3220e4eeb29a622a95db
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2022-04-15 10:36:39 +0800

    changes

--- bmkg/models/transx/transr.py
+++ bmkg/models/transx/transr.py
@@ -18,8 +18,7 @@ class TransR(TransX):
 		nn.init.xavier_uniform_(self.ent_embed.weight.data)
 		nn.init.xavier_uniform_(self.rel_embed.weight.data)
 
-		#self.transfer_matrix = nn.Embedding(config.rel_size, self.dim_e * self.dim_r)
-		self.transfer_matrix = nn.Embedding(config.rel_size, self.dim_e * self.dim_r * config.ent_size)
+		self.transfer_matrix = nn.Embedding(config.rel_size, self.dim_e * self.dim_r)
 		if not self.rand_init:
 			identity = torch.zeros(self.dim_e, self.dim_r)
 			for i in range(min(self.dim_e, self.dim_r)):
