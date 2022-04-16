bcb9a49c346170ff333c274758d01a8f1af7bfa5
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2022-04-16 09:44:15 +0800

    Update transe.py

--- bmkg/models/transx/transe.py
+++ bmkg/models/transx/transe.py
@@ -13,7 +13,7 @@ class TransE(TransX):
         score = torch.norm(score, p=self.p_norm, dim=-1)
         return score
 
-    def soreing_function_2(self, h, r, t, *_):
+    def soreing_function_2(self, heads, rels, tails, *_):
         #score function for valid & test remain the same in TransE
         score = self.ent_embed(heads) + self.rel_embed(rels) - self.ent_embed(tails)
         score = torch.norm(score, p=self.p_norm, dim=-1)
