7bad0e26ba075c63fbd3b5927eec574967a37487
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2022-04-17 15:04:14 +0800

    Update transr.py

--- bmkg/models/transx/transr.py
+++ bmkg/models/transx/transr.py
@@ -4,59 +4,61 @@ import torch.nn.functional as F
 import argparse
 from .transx import TransX
 
+
 class TransR(TransX):
 
-	def __init__(self,config: argparse.Namespace, norm_flag = True, rand_init = False, margin = None):
-		super(TransR, self).__init__(config)
-		
-		self.dim_e = config.dim
-		self.dim_r = config.dim
-		self.norm_flag = norm_flag
-		self.rand_init = rand_init
-		self.ent_size = config.ent_size
+    def __init__(self, config: argparse.Namespace, norm_flag=True, rand_init=False):
+        super(TransR, self).__init__(config)
 
-		self.transfer_matrix = nn.Embedding(config.rel_size, self.dim_e * self.dim_r)
-		if not self.rand_init:
-			identity = torch.zeros(self.dim_e, self.dim_r)
-			for i in range(min(self.dim_e, self.dim_r)):
-				identity[i][i] = 1
-			identity = identity.view(self.dim_r * self.dim_e)
-			for i in range(config.rel_size):
-				self.transfer_matrix.weight.data[i] = identity
-		else:
-			nn.init.xavier_uniform_(self.transfer_matrix.weight.data)
+        self.dim_e = config.dim
+        self.dim_r = config.dim
+        self.norm_flag = norm_flag
+        self.rand_init = rand_init
+        self.ent_size = config.ent_size
 
-		if margin is not None:
-			self.margin = nn.Parameter(torch.Tensor([margin]))
-			self.margin.requires_grad = False
-			self.margin_flag = True
-		else:
-			self.margin_flag = False
+        self.transfer_matrix = nn.Embedding(config.rel_size, self.dim_e * self.dim_r)
+        if not self.rand_init:
+            identity = torch.eye(self.dim_e, self.dim_r)
+            self.transfer_matrix.weight.data = identity \
+                .expand(config.rel_size, self.dim_e, self.dim_r) \
+                .view(config.rel_size, self.dim_e * self.dim_r)
+        else:
+            nn.init.xavier_uniform_(self.transfer_matrix.weight.data)
 
-	def scoring_function(self, heads, rels, tails, *_):
-		"""
-		:param heads: torch.Tensor() shaped (batch_size), containing the id for the head entity.
-		:param rels: torch.Tensor() shaped (batch_size), containing the id for the relation.
-		:param tails: torch.Tensor() shaped (batch_size), containing the id for the tail entity.
-		:return: torch.Tensor() shaped (batch_size). The individual score for each
-		"""
+    def scoring_function(self, heads, rels, tails, *_):
+        """
+        :param heads: torch.Tensor() shaped (batch_size), containing the id for the head entity.
+        :param rels: torch.Tensor() shaped (batch_size), containing the id for the relation.
+        :param tails: torch.Tensor() shaped (batch_size), containing the id for the tail entity.
+        :return: torch.Tensor() shaped (batch_size). The individual score for each
+        """
+        # (batch_size, 1, dim_e) or (batch_size, dim_e)
+        h: torch.Tensor = self.ent_embed(heads)
+        # (batch_size, 1, dim_e) or (batch_size, dim_e)
+        r: torch.Tensor = self.rel_embed(rels)
+        # (1, ent_size, dim_e) or (batch_size, dim_e)
+        t: torch.Tensor = self.ent_embed(tails)
+        # (batch_size, 1, dim_e * dim_e) or (batch_size, dim_e * dim_e)
+        prj: torch.Tensor = self.transfer_matrix(rels)
+        # (batch_size, 1, dim_e, dim_e) or (batch, dim_e, dim_e)
+        prj = prj.view(*prj.size()[:-1], self.dim_e, self.dim_r)
 
-		h: torch.Tensor = self.ent_embed(heads)
-		r: torch.Tensor = self.rel_embed(rels)
-		t: torch.Tensor = self.ent_embed(tails)
-		prj: torch.Tensor = self.transfer_matrix(rels)
-		prj = prj.view(*prj.size()[:-1], self.dim_e, self.dim_r)
+        # (batch_size, 1, 1, dim_e) or (batch_size, 1, dim_e)
+        h = torch.unsqueeze(h, -2)
+        # (1, ent_size, 1, dim_e) or (batch_size, 1, dim_e)
+        t = torch.unsqueeze(t, -2)
 
-		h = torch.unsqueeze(h, -2)
-		t = torch.unsqueeze(t, -2)
+        # (batch_size, 1, 1, dim_e) or (batch_size, 1, dim_e)
+        h = torch.matmul(h, prj)
+        # (1, ent_size, 1, dim_e) or (batch_size, 1, dim_e)
+        t = torch.matmul(t, prj)
 
-		h = torch.matmul(h, prj)
-		t = torch.matmul(t, prj)
+        # (batch_size, 1, dim_e) or (batch_size, dim_e)
+        h = torch.squeeze(h, -2)
+        # (1, ent_size, dim_e) or (batch_size, dim_e)
+        t = torch.squeeze(t, -2)
 
-
-		h = torch.squeeze(h, -2)
-		t = torch.squeeze(t, -2)
-
-		score = h + r - t
-		score = torch.norm(score, p=self.p_norm, dim=-1)
-		return score
+        # (batch_size, ent_size, dim_e) or (batch_size, dim_e)
+        score = h + r - t
+        score = torch.norm(score, p=self.p_norm, dim=-1)
+        return score
