8e64155e9720ff1d025ccc7eacd5520a7bf0710f
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2022-05-09 10:59:29 +0800

    Update transx.py

--- bmkg/models/transx/transx.py
+++ bmkg/models/transx/transx.py
@@ -40,7 +40,7 @@ class TransX(BMKGModel, ABC):
         self.p_norm = config.p_norm
         with torch.no_grad():
             ###todo:bmtrain norm
-            self.rel_embed.weight /= torch.norm(self.rel_embed.weight, p=self.p_norm, dim=-1)[:, None]
+            self.rel_embed.weight /= torch.nn.Parameter(torch.norm(self.rel_embed.weight, p=self.p_norm, dim=-1)[:, None]).cuda()
 
     @abstractmethod
     def scoring_function(self, heads, rels, tails, *args):
